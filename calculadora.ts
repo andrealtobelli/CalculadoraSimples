88fe5f280b8e1f275297fce383d5a1a47f63e64f
andrealtobelli <andrerogerio03@gmail.com>
2025-02-27 10:55:43 -0300

    Funções de operação da calculadora criadas.

--- calculadora.ts
+++ calculadora.ts
@@ -3,3 +3,23 @@ function calculadoraNova (n1 : number, n2 : number,
 
         return operacao(n1,n2)
 }
+
+function somarNovo (n1 : number, n2 : number) : number{
+    return n1 + n2
+}
+
+function subtrairNovo (n1 : number, n2 : number) : number{
+    return n1 - n2
+}
+
+function multiplicarNovo (n1 : number, n2 : number) : number{
+    return n1 * n2
+}
+
+function dividirNovo (n1 : number, n2 : number) : number{
+    return n1 / n2
+}
+
+function potenciacaoNovo (n1 : number, n2 : number) : number{
+    return n1 ** n2
+}
