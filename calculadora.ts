e4c5d7f240b9b94dcc5f2424ffff1420b5b95ab0
andrealtobelli <andrerogerio03@gmail.com>
2025-02-27 10:57:28 -0300

    Testes de execução para cada operação executado.

--- calculadora.ts
+++ calculadora.ts
@@ -23,3 +23,18 @@ function dividirNovo (n1 : number, n2 : number) : number{
 function potenciacaoNovo (n1 : number, n2 : number) : number{
     return n1 ** n2
 }
+
+let r1 = calculadoraNova( 10,10,somarNovo)
+console.log("Somar 10 + 10: ", r1);
+
+let r2 = calculadoraNova( 10,10,subtrairNovo)
+console.log("Subtrair 10 - 10: ", r2);
+
+let r3 = calculadoraNova( 10,10,multiplicarNovo)
+console.log("Multiplicar 10 * 10: ", r3);
+
+let r4 = calculadoraNova( 10,10,dividirNovo)
+console.log("Dividir 10 / 10: ", r4);
+
+let r5 = calculadoraNova( 2,16,potenciacaoNovo)
+console.log("Potenciação 10 ** 10: ", r5);
